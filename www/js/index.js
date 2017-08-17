391e624bce4a87b4e475deb3e99bd481ff59c684
Damian Dzierzak <damian.dzierzak@miquido.com>
2017-08-17 10:18:18 +0200

    refactor

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+var myApp = new Framework7({
+   modalTitle: 'Framework7',
+   material: true,
+});
+var $$ = Dom7;
+var mainView = myApp.addView('.view-main', {});
+var rightView = myApp.addView('.view-right', {
+   name: 'right'
+});
