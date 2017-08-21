141fe79b3b570bec2e1622accc8886490a217b4c
Damian Dzierzak <damian.dzierzak@miquido.com>
2017-08-21 21:30:18 +0200

    add onclick

--- www/js/index.js
+++ www/js/index.js
@@ -38,13 +38,13 @@ function checkFavourites() {
             items: [
                 {
                     title: "Tu się pojawią ulubione",
-                    id: "0"
+                    id: "-1"
 					},
 				],
             // Template 7 template to render each item
             template: '<li class="item-content">' +
                 '<div class="item-inner">' +
-                '<div class="item-title">{{title}}</div><div class="remove" onclick="removeItem({{id}})">X</div>' +
+                '<div class="item-title" onclick="onItemClicked({{id}})">{{title}}</div><div class="remove" onclick="removeItem({{id}})">X</div>' +
                 '</div>' +
                 '</li>'
         });
@@ -64,6 +64,74 @@ function checkFavourites() {
         $$('#no-favourite-container').hide()
         $$('#favourite-list').show()
     }
+}
+
+function onItemClicked(id) {
+    if (id != "-1") {
+        console.log("item clicked" + id);
+        myApp.popup('.popup');
+        $$('.popup').on('popup:opened', function () {
+            console.log("popup opened");
+            performFavouriteRequest("Batorego", "4");
+        });
+
+    }
+}
+
+
+function performFavouriteRequest(stopName, lineNr) {
+    var queryString =
+        'http://www.ttss.krakow.pl/internetservice/services/passageInfo/stopPassages/stop?stop=' + stopsNamesIdsMap[stopName] + '&mode=departure';
+
+    $$.getJSON(queryString, function (results) {
+
+        var resultText = "";
+        var arr = [];
+        var direction = [];
+        for (i = 0; i < results.actual.length; i++) {
+            if (results.actual[i].patternText == lineNr) {
+                console.log(parseTime(results.actual[i].mixedTime));
+                arr.push(parseTime(results.actual[i].mixedTime));
+                direction.push(results.actual[i].direction);
+            }
+        }
+        console.log("getJSON in favourite:" + myApp);
+        var myList = myApp.virtualList('.list-block.virtual-list.dwa', {
+            // Array with items data
+            items: [
+                {
+                    title: "Tu się pojawią odjazdy"
+					},
+				],
+            // Template 7 template to render each item
+            template: '<li class="item-content">' +
+                '<div class="item-inner">' +
+                '<div class="item-title">{{title}}</div>' +
+                '</div>' +
+                '</li>'
+        });
+        console.log("getJSON in favourite: po liscie");
+        if (0 < arr.length) {
+            console.log("getJSON in favourite:>0");
+            for (i = 0; i < arr.length; i++) {
+                if (0 == i) {
+                    myList.replaceItem(0, {
+                        title: "Kierunek: " + direction[i] + " Odjazd za: " + arr[i]
+                    });
+                } else {
+                    myList.appendItem({
+                        title: "Kierunek: " + direction[i] + " Odjazd za: " + arr[i]
+                    });
+                }
+            }
+        } else {
+            console.log("getJSON in favourite:else");
+            myList.replaceItem(0, {
+                title: "Nie znaleziono odjazdów"
+            });
+        }
+    });
+
 }
 
 function removeItem(id) {
